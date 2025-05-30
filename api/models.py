1461810c67d2440db6f199324e112da5e163c060
httpsMansoor <malangbaba535@gmail.com>
2025-05-30 15:01:32 +0500

    feat: add user field to Singer model for authentication

--- api/models.py
+++ api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class Singer(models.Model):
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
