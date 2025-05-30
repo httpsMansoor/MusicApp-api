0197d39c7b1396b0403d05cfc101b2e5f304c55a
httpsMansoor <malangbaba535@gmail.com>
2025-05-30 14:40:10 +0500

    refactor: change song duration to minutes and seconds format

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Singer(models.Model):
     name = models.CharField(max_length=100)
@@ -9,8 +10,16 @@ class Singer(models.Model):
 
 class Songs(models.Model):
     title = models.CharField(max_length=250)
-    duration = models.IntegerField()
+    duration_minutes = models.PositiveIntegerField()
+    duration_seconds = models.PositiveIntegerField()
     singer = models.ForeignKey(Singer, on_delete=models.CASCADE, related_name='songs', null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def get_duration_display(self):
+        return f"{self.duration_minutes}:{self.duration_seconds:02d}"
+
+    def clean(self):
+        if self.duration_seconds >= 60:
+            raise ValidationError("Seconds must be less than 60")
