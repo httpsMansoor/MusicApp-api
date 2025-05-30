8ee62f60131ab3670e600afbbeae7a86a80fc26e
httpsMansoor <malangbaba535@gmail.com>
2025-05-30 15:05:06 +0500

    refactor: simplify song serializer to auto-set singer

--- api/serializers.py
+++ api/serializers.py
@@ -31,8 +31,7 @@ class SingerSerializer(serializers.ModelSerializer):
 
 class SongSerializer(serializers.ModelSerializer):
     singer_name = serializers.CharField(source='singer.name', read_only=True)
-    singer = serializers.PrimaryKeyRelatedField(queryset=Singer.objects.all(), write_only=True)
     
     class Meta:
         model = Songs
-        fields = ['id', 'title', 'duration_minutes', 'duration_seconds', 'singer', 'singer_name']
+        fields = ['id', 'title', 'duration_minutes', 'duration_seconds', 'singer_name']
